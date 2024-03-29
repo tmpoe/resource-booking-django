7f9b00082a5d3ade5f9f94efa86da4d3b5e5add5
tmpITK <torok.mark.patrik@gmail.com>
2024-03-29 09:30:27 +0100

    Add models to admin and use user in booking

--- resource_booking/booking/admin.py
+++ resource_booking/booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Booking, Room, Site
+
 # Register your models here.
+admin.site.register(Site)
+admin.site.register(Room)
+admin.site.register(Booking)
--- resource_booking/booking/models.py
+++ resource_booking/booking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,7 +22,7 @@ class Booking(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    user = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.room} - {self.start_time} - {self.end_time}"
