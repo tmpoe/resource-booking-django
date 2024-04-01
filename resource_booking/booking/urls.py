2629450fb9efd3ac1c0c86b3bc479c14b2a6a3cc
tmpITK <torok.mark.patrik@gmail.com>
2024-04-01 08:47:34 +0200

    Refactor urls

--- resource_booking/booking/urls.py
+++ resource_booking/booking/urls.py
@@ -1,7 +1,22 @@
-from django.urls import path
+from django.urls import include, path
 
 from booking import views
 
+from rest_framework import routers
+
+from booking import views as booking_views
+from users import views as user_views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', user_views.UserViewSet)
+router.register(r'groups', user_views.GroupViewSet)
+router.register(r'rooms', booking_views.RoomViewSet)
+router.register(r'sites', booking_views.SiteViewSet)
+router.register(r'bookings', booking_views.BookingViewSet)
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name="index"),
+    path('', include(router.urls)),
+    
 ]
--- resource_booking/resource_booking/urls.py
+++ resource_booking/resource_booking/urls.py
@@ -1,21 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from rest_framework import routers
-
-from booking import views as booking_views
-from users import views as user_views
-
-router = routers.DefaultRouter()
-router.register(r'users', user_views.UserViewSet)
-router.register(r'groups', user_views.GroupViewSet)
-router.register(r'bookings', booking_views.BookingViewSet)
-router.register(r'rooms', booking_views.RoomViewSet)
-router.register(r'sites', booking_views.SiteViewSet)
-
-
 urlpatterns = [
     path("booking/", include("booking.urls")),
-    path('', include(router.urls)),
     path("admin/", admin.site.urls),
 ]
